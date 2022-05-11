f398e5f1c296c2a50683918d85e9498fe94c2c59
folomeev1980 <a.a.folomeev80@gmail.com>
2022-05-11 11:22:38 +0300

    some fix

--- invoiceToPDF.py
+++ invoiceToPDF.py
@@ -2,24 +2,26 @@ from xhtml2pdf import pisa
 import datetime
 from core import get_string_by_number as number_to_string
 import os
+
+# пока не удалять:
 # from django.template.loader import get_template
 # from django.template import Context
 # from django.template import Template
 # from django.conf import settings
 # import django
 
-
-
+# Модуль преобразующий пути к локальным файлам
 def fetch_pdf_resources(uri, rel):
     #print('fetch', uri)
     if uri[0:5] == 'media':
         path = os.getcwd()+"/media" + uri[5:]
-        print(path)
+       # print(path)
     else:
         path = None
     return path
 
-
+#Конверитует sample.html в pdf файл, на вход модуля подается словарь с параметрами для
+# формирования invoice.pdf
 def toPDF(**kwarg):
     result = open('invoice.pdf', "w+b")
     with open('sample.html', 'r', encoding='utf-8-sig') as f:
@@ -35,14 +37,17 @@ def toPDF(**kwarg):
         words_price=number_to_string(
             float(int(kwarg['qty']) * float(kwarg['price']))))
 
+    # пока не удалять:
     # template = Template(html)
     # context = Context({})
     # html = template.render(context)
     # print(html)
-    pisa.pisaDocument(html.encode("UTF-8"), result, encoding='UTF-8', link_callback=fetch_pdf_resources)
+    pisa.pisaDocument(html.encode("UTF-8"), result,
+                      encoding='UTF-8', link_callback=fetch_pdf_resources)
 
 
 if __name__ == '__main__':
+    # пока не удалять:
 
     # MEDIA_ROOT = os.getcwd()
     # MEDIA_URL = '/media/'
@@ -56,9 +61,11 @@ if __name__ == '__main__':
     # settings.configure(TEMPLATES=TEMPLATES)
     # django.setup()
 
+
+    # Входные параметры
     input_json = {
         'invoice_number': '1',
-        'contragent': "ЗАО «УНИГРА», 121099, Москва, ул.Новый Арбат, 34, стр.1, помещение 1, этаж4, комнаты 7,8, ИНН 7704245553, КПП 771932001, р/с 40702810900000000195, банк ООО 'ДОЙЧЕ БАНК', г. Москва, кор.счет 30101810100000000101, БИК яяя",
+        'contragent':  "ЗАО «УНИГРА», 121099, Москва, ул.Новый Арбат, 34, стр.1, помещение 1, этаж4, комнаты 7,8, ИНН 7704245553, КПП 771932001, р/с 40702810900000000195, банк ООО 'ДОЙЧЕ БАНК', г. Москва, кор.счет 30101810100000000101, БИК яяя",
         'price': '3750.12',
         'qty': '3',
         'invoice_date': datetime.datetime.now().strftime("%d.%m.%Y")}
